24a49a5a0e5d03992fd48d207d8beb4689d57a83
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-11-20 15:48:14 -0800

    Update axios.js

--- frontend/src/Api/axios.js
+++ frontend/src/Api/axios.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api", // backend URL
+// });
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Add JWT token to headers
