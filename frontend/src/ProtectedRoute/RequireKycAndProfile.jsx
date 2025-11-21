cf29567d25e4ed90d4ef08ed0eee24dfa0208272
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-11-21 11:34:28 -0800

    Update RequireKycAndProfile.jsx
    requre only kyc data not user profile

--- frontend/src/ProtectedRoute/RequireKycAndProfile.jsx
+++ frontend/src/ProtectedRoute/RequireKycAndProfile.jsx
@@ -11,17 +11,17 @@ const ShopGuard = ({ children }) => {
     const checkPermissions = async () => {
       try {
         const kyc = await axios.get("/api/kyc/my-kyc");
-        const profile = await axios.get("/api/profile/my-profile");
+        // const profile = await axios.get("/api/profile/my-profile");
 
-        if (!kyc.data || !profile.data) {
+        if (!kyc.data ) {
           alert("Please complete your KYC and Profile first!");
-          return navigate("/complete-kyc");
+          return navigate("/RecognitionForm");
         }
 
         setAllowed(true);
       } catch (err) {
         alert("Please complete your KYC and Profile first!");
-        navigate("/complete-kyc");
+        navigate("/RecognitionForm");
       } finally {
         setLoading(false);
       }
