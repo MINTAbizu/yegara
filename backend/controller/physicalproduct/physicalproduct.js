a8950b21bc111b1e6d413663ec8bcdcdd5bbcf0d
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-11-27 13:17:55 -0800

    Update physicalproduct.js

--- backend/controller/physicalproduct/physicalproduct.js
+++ backend/controller/physicalproduct/physicalproduct.js
@@ -7,7 +7,7 @@ export const addphyshicalProduct = async (req, res) => {
     if (!req.file) return res.status(400).json({ message: "Product image is required" });
 
     
-    const image = `/uploads/digitalProducts/${req.file.filename}`; // store relative path
+    const image = `/uploads/physicalProducts/${req.file.filename}`; // store relative path
 
     const newProduct = new physicalProduct({
       productName,
@@ -15,6 +15,7 @@ export const addphyshicalProduct = async (req, res) => {
       description,
       image,
       telegram,
+      seller: req.user._id,
       drive,
       dropbox,
       productLink,
