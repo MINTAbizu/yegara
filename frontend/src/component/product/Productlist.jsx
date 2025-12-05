07bfba207a5bfc9f5e99c62528a8c81c028fc726
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-12-05 12:08:39 -0800

    Update Productlist.jsx

--- frontend/src/component/product/Productlist.jsx
+++ frontend/src/component/product/Productlist.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './productlist.css';
-// import product1 from '../../assets/image/product1.jpg';
-// import product2 from '../../assets/image/product2.jpg';
-// import product3 from '../../assets/image/product3.jpg';
+import product1 from '../../assets/image/test.jpg';
+import product2 from '../../assets/image/test5.png';
+import product3 from '../../assets/image/test5.png';
 
 const products = [
-  { id: 1, name: 'Digital Product 1', image: '' },
-  { id: 2, name: 'Digital Product 2', image: '' },
-  { id: 3, name: 'Digital Product 3', image: '' },
+  { id: 1, name: 'Digital Product 1', image: product1 },
+  { id: 2, name: 'Digital Product 2', image: product2},
+  { id: 3, name: 'Digital Product 3', image: product3 },
   { id: 3, name: 'Digital Product 3', image: '' },
   // { id: 4, name: 'Digital Product 4', image: "" },
   // { id: 5, name: 'Digital Product 5', image:"" },
