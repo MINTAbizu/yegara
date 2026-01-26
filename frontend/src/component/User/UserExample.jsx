09c260fe399ef15085a3dc39ada3c700d378f3fc
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2026-01-26 15:02:52 -0800

    Update UserExample.jsx

--- frontend/src/component/User/UserExample.jsx
+++ frontend/src/component/User/UserExample.jsx
@@ -20,7 +20,7 @@ const UserExample = () => {
       const token = localStorage.getItem("adminToken");
 
       const res = await axios.get(
-        `${API_URL}/api/profile/ourusers?page=${pageNum}&limit=${limit}`,
+        `${API_URL}/api/users/ouruser?page=${pageNum}&limit=${limit}`,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
