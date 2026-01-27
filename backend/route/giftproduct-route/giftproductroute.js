c829c88a7afb62940b38b7a7ecde7595d96551f0
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2026-01-27 11:30:53 -0800

    gift product middelware and route

---
+++ backend/route/giftproduct-route/giftproductroute.js
@@ -0,0 +1,35 @@
+
+import express from "express";
+import upload from "../../middleware/giftproductmiddlewarw/gift-middel.js";
+import {
+  addgiftproduct,
+//   getApprovedProducts,
+//   getAllProductsAdmin,
+//   getSingleProduct,
+//   toggleStatus,
+//   getDigitalProductById,
+//   getDigitalProductWithSellerStats
+} from "../../controller/giftproduct/Giftproduct.js";
+import { protect } from "../../middleware/authMiddleware.js";
+
+const router = express.Router();
+// CREATE PRODUCT
+router.post("/create", protect, upload.single("image"), addgiftproduct);
+
+// // USER PAGES
+// router.get("/", getApprovedProducts);
+// router.get("/:id", getSingleProduct);
+
+
+
+
+// // ADMIN PAGES
+// router.get("/admin/all", getAllProductsAdmin);
+// router.patch("/admin/toggle/:id", toggleStatus);
+
+
+// // details
+
+// router.get("/:id", getDigitalProductById);
+// router.get("/:id", getDigitalProductWithSellerStats);
+// export default router;
