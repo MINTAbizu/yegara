93c34a9d5aba1904d54e6fb42b47fd48fea922c6
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2026-02-01 13:11:18 -0800

    Update user.controller.js

--- backend/controller/user.controller/user.controller.js
+++ backend/controller/user.controller/user.controller.js
@@ -188,6 +188,7 @@ const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const strongPasswordRegex =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
 
+  
 /**
  * Helper: generate JWT
  */
